d046ae1d8bae98a4e70b84971e12aef82fd4ca4c
Mark Turingan <mnturingan@student.apc.edu.ph>
2023-02-17 18:27:29 +0800

    minimal fix

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -23,10 +23,11 @@
 
 <div class="wrapper">
     <header>
-        <b><a href="home">mnturingan</a></b>
+        <b>mnturingan</a></b>
         <nav>
             <ul>
                 <li><a href="home">Home</a></li>
+                <li><a href="gallery">Gallery</a></li>
                 <li><a href="about">About Me</a></li>
                 <li><a href="resources">Resources</a></li>
             </ul>
